074d35df11877914fceae37230f27f622bef5e67
Sam Wong <sam.wong.uk@gmail.com>
2014-12-12 22:36:58 +0000

    Initial Commit

---
+++ TwitterCloneV2Tests/TwitterCloneV2Tests.m
@@ -0,0 +1,40 @@
+//
+//  TwitterCloneV2Tests.m
+//  TwitterCloneV2Tests
+//
+//  Created by Sam Wong on 12/12/2014.
+//  Copyright (c) 2014 sam wong. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import <XCTest/XCTest.h>
+
+@interface TwitterCloneV2Tests : XCTestCase
+
+@end
+
+@implementation TwitterCloneV2Tests
+
+- (void)setUp {
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample {
+    // This is an example of a functional test case.
+    XCTAssert(YES, @"Pass");
+}
+
+- (void)testPerformanceExample {
+    // This is an example of a performance test case.
+    [self measureBlock:^{
+        // Put the code you want to measure the time of here.
+    }];
+}
+
+@end
